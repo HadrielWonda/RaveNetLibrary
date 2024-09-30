2e884b68c8eea9ebec6de880b631d4e4ff246926
HadrielWonda <olawalebash2017@gmail.com>
2024-09-30 14:02:52 +0100

    banks/fees

---
+++ src/Banks/BankService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RaveNetLibrary.Banks
+{
+    public static class BankService
+    {
+        private static readonly RavePayApiRequest RavePayRequest = new RavePayApiRequest();
+
+        public static async Task<IEnumerable<Bank>> GetBankDebitDirectList()
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, Enpoints.GetDirectBankDebitList);
+            return await RavePayRequest.Request<IEnumerable<Bank>>(requestMessage);
+        }
+
+        /// <summary>
+        /// Gets the bank transfer list.
+        /// </summary>
+        /// <returns>The bank transfer list.</returns>
+        /// <param name="country">The Country To Get Bank List From Within Africa ex NG,GH,KE,UG,TZ.</param>
+        public static async Task<RaveApiResponse<BankTransferListResponse>> GetBankTransferList(string country,RavePayConfig config)
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"{Enpoints.GetBankTransferList}{country}?public_key={config.PbfPubKey}");
+            return await RavePayRequest.Request<RaveApiResponse<BankTransferListResponse>>(requestMessage);
+        }
+    }
+}
---
+++ src/Fees/RaveFeeService.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RaveNetLibrary
+{
+    public static class RaveFeeService
+    {
+        private static readonly RavePayApiRequest<RaveApiResponse<RaveFeesData>, RaveFeesData> ApiRequest =
+            new RavePayApiRequest<RaveApiResponse<RaveFeesData>, RaveFeesData>(new RavePayConfig(false));
+
+        public static async Task<RaveApiResponse<RaveFeesData>> GetFees(GetFeesParams feesParams)
+        {
+            var paramsBody = new StringContent(JsonConvert.SerializeObject(feesParams), Encoding.UTF8, "application/json");
+            var httpMessage = new HttpRequestMessage(HttpMethod.Post, Enpoints.GetFees) { Content = paramsBody };
+            return await ApiRequest.Request(httpMessage);
+        }
+    }
+}
